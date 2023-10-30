3e201ee06d279d412cd3369171246c4d3bd98a85
iporkii <38855188+iporkii@users.noreply.github.com>
2023-10-30 10:32:02 +1030

    Update Node.py

--- Node.py
+++ Node.py
@@ -6,14 +6,6 @@ class Node:
         self.next = None  # next node in list
         self.book_next = None  # next node of same book as current
         self.next_frequent_search = None # for part 2 analysis
-        self.book_num = None
-        temp = name.replace("book_","")
-        temp = temp.replace(".txt","")
-        self.book_num = int(temp)
 
     def print(self):
         print(f"Data: {self.data}, Name: {self.name}")
-    
-    def getInfo(self):
-        return f"Data: {self.data}, Name: {self.name}"
-    
